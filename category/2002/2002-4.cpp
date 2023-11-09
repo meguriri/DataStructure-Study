f46c5a3c7a60b35d25bcf4490b2341a773f3ebaf
meguriri <1310500986@qq.com>
2023-11-09 20:46:01 +0800

    Changed project structure

--- category/2002/2002-4.cpp
+++ category/2002/2002-4.cpp
@@ -2,37 +2,7 @@
   二叉链表存储的二叉树，从右向左删除叶子节点，并将节点值放到一个数组中
   1 2 4 -1 -1 5 -1 -1 3 6 -1 -1 7 -1 -1
 */
-
-#include<cstdio>
-#include<cstdlib>
-
-typedef struct node{
-  int d;
-  node *l,*r;
-}bitNode,*bitTree;
-
-bitTree creatBitTree(){
-  bitNode *t=new(node);
-  int x;
-  scanf("%d",&x);
-  if(x==-1){
-    return nullptr;
-  }
-  t->d=x;
-  t->l=creatBitTree();
-  t->r=creatBitTree();
-  return t;
-}
-
-void preOrderTraversal(bitTree t){
-  if(t==nullptr){
-    return;
-  }
-  printf("%d ",t->d);
-  preOrderTraversal(t->l);
-  preOrderTraversal(t->r);
-}
-
+#include"bitTree.h"
 
 void deleteLeaf(bitTree &t,int a[],int &cnt){
   if(t->l==nullptr&&t->r==nullptr){
