b1c7f80c99f9a85f2abff9118feb47a5f1179560
meguriri <1310500986@qq.com>
2023-12-04 00:02:47 +0800

    add bstTree

--- category/todo/删除bst树中所有小于x的节点.cpp
+++ category/todo/删除bst树中所有小于x的节点.cpp
@@ -1,6 +1,46 @@
+/*
+  eg1.
+  10 8 14 13 25 
+  2 6 11 15 30 16 17 5
+*/
 #include"bstTree.h"
 
+void FreeNode(bstTree t){
+  if(t==nullptr){
+    return;
+  }
+  FreeNode(t->l);
+  FreeNode(t->r);
+  t->l=nullptr;
+  t->r=nullptr;
+  delete(t);
+}
+
+void deleteXNode(bstTree &t,int x){
+  if(t==nullptr){
+    return;
+  }
+  if(t->d==x){
+    FreeNode(t->l);
+    t->l=nullptr;
+  }else if(t->d>x){
+    deleteXNode(t->l,x);
+  }else{
+    FreeNode(t->l);
+    t->l=nullptr;
+    bstNode *pre=t;
+    t=t->r;
+    delete(pre);
+    deleteXNode(t,x);
+  }
+}
+
 int main(){
-  
+  bstTree t=creatBstTree(13);
+  inOrderTraversal(t);
+  printf("\n");
+  deleteXNode(t,12);
+  inOrderTraversal(t);
+  printf("\n");
   return 0;
 }
